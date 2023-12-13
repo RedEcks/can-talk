88fdf9e564cd86466674d27fede7f089c9a8f5d2
Xavier Dunkley <xavierxv18@gmail.com>
2023-12-13 11:04:10 -0500

    added test data and create first endpoints to render on webpage

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require("express")
+const { chats } = require("./data/data")
 
 
 const app = express()
 
 app.get('/', (req, res) =>{
-    
+    res.send("API is running")
+})
+
+app.get('/api/chat', (req,res)=>{
+    res.send(chats)
 })
 
 app.listen(3001, console.log("Server is on port 3001"))
