e7dea691eefa5bcf36cc4414928dd744aca75db2
Xavier Dunkley <xavierxv18@gmail.com>
2023-12-13 12:43:28 -0500

    created home and chat components and successfully added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,43 @@
-import logo from './logo.svg';
+//import { Button } from '@chakra-ui/react'
 import './App.css';
+import {createBrowserRouter, createRoutesFromElements, Link, Route, RouterProvider, Outlet} from 'react-router-dom'
+import Home from "./components/Home"
+import ChatPage from './components/ChatPage';
+
 
 function App() {
+
+  const Root=()=>{
+    return(
+      <>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/chat"> ChatPage</Link>
+        </div>
+        <div>
+          <Outlet/>
+        </div>
+      </>
+    )
+  }
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Root/>}>
+        <Route path='/' index element={<Home/>}/>
+        <Route path='/chat' element={<ChatPage/>}/>
+      </Route>
+    )
+  )
+
+
   return (
     <div className="App">
-      Hello World!
+      <RouterProvider router={router}/>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ frontend/src/components/ChatPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Chat(){
+
+    return(
+        <div>ChatPage</div>
+    )
+}
+
+export default Chat
