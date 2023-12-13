e14721bd94d050773e3ed5479886b8ecc51f431e
Xavier Dunkley <xavierxv18@gmail.com>
2023-12-13 11:30:12 -0500

    started frontend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
 const express = require("express")
 const { chats } = require("./data/data")
+const dotenv = require('dotenv')
 
 
 const app = express()
+dotenv.config()
 
 app.get('/', (req, res) =>{
-    res.send("API is running")
+    res.send("It's alive!")
 })
 
 app.get('/api/chat', (req,res)=>{
     res.send(chats)
 })
 
-app.listen(3001, console.log("Server is on port 3001"))
+app.get('/api/chat/:id', (req,res)=>{
+    //console.log(req.params.id)
+    const singleChat = chats.find(c=>c._id ===req.params.id)
+    res.send(singleChat)
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(3001, console.log(`Server is on port ${PORT}`))
