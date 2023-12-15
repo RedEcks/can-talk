d9dd4da2c297828895a8455ce549722cfc16129a
Xavier Dunkley <xavierxv18@gmail.com>
2023-12-15 15:48:04 -0500

    created user routes, registration api, JWT, login api, and password encryption with bcrypt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,13 +1,29 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+
+
 
 const userSchema = mongoose.Schema({
-    name:{type:String, require:true},
-    email: {type: String, require: true},
+    name:{type:String, required:true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    pic: {type: String, required: true, default: "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"},
+    pic: {type: String, default: "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"},
 }, {timestamps:true})
 
 
+userSchema.methods.matchPassword=async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified){
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
 const User = mongoose.model("User", userSchema)
 
 
--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,18 @@ const express = require("express")
 const { chats } = require("./data/data")
 const dotenv = require('dotenv')
 const connectDB = require("./config/db")
+const userRoutes = require("./routes/userRoutes")
 
 const app = express()
 dotenv.config()
 connectDB()
+app.use(express.json()) // to accept JSON Data
 
 app.get('/', (req, res) =>{
     res.send("It's alive!")
 })
 
-app.get('/api/chat', (req,res)=>{
-    res.send(chats)
-})
-
-app.get('/api/chat/:id', (req,res)=>{
-    //console.log(req.params.id)
-    const singleChat = chats.find(c=>c._id ===req.params.id)
-    res.send(singleChat)
-})
+app.use('/api/user', userRoutes)
 
 const PORT = process.env.PORT || 3001
 
